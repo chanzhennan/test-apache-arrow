11a1c83d5c9468bdcda407d357d7cfcb88277417
vkhristenko <vdkhristenko1991@gmail.com>
2018-10-17 15:51:39 +0200

    moved old apache arrow tests

---
+++ simple_tests/hello_world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "test_include.hpp"
+
+int main() {
+    X x{15};
+
+    std::cout << "hello world" << std::endl;
+ }
---
+++ simple_tests/test_include.hpp
@@ -0,0 +1,8 @@
+class X {
+private:
+    int x;
+
+public:
+    X() : x{0} {}
+    X(int i) : x{i} {}
+};
